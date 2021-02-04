abca93778cd0b58e933d539a4bd28b44f760c836
bigmuncha <mr.munchaev@mail.ru>
2021-02-04 23:00:07 +0300

    Cmake

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <boost/asio.hpp>
-
+#include <vector>
+#include <thread>
+#include <functional>
+#include <cstring>
 using boost::asio::ip::tcp;
+
+void workerfunc(tcp::socket& sock){
+    char buf[128];
+    std::memset(buf, 0, 128);
+    sock.read_some(boost::asio::buffer(buf));
+    std::cout <<buf;
+    boost::asio::write(sock,boost::asio::buffer("OMAR"));
+    sock.shutdown(tcp::socket::shutdown_both);
+    sock.close();
+}
+
+
 
 int main(int argc, char *argv[]) {
 
     boost::asio::io_context io_context;
 
     tcp::socket socket(io_context);
+
+    std::vector<std::thread> pool_th;
+    for(int i=0;i < 10 ; i++){
+        pool_th.push_back(std::thread(workerfunc(tcp::socket &sock)));
+    }
 
     tcp::acceptor acceptor(io_context);
 
@@ -17,15 +37,9 @@ int main(int argc, char *argv[]) {
     acceptor.listen();
 
     for(;;){
-        socket = acceptor.accept();
-        //socket.set_option(tcp::acceptor::reuse_address(true));
-        char buf[64];
-        socket.read_some(boost::asio::buffer(buf));
 
-        socket.write_some(boost::asio::buffer("OMAR"));
-        std::cout <<"con\n";
-        socket.shutdown(tcp::socket::shutdown_both);
-        socket.close();
+        socket = acceptor.accept();
+        workerfunc(socket);
     }
     return 0;
 }
