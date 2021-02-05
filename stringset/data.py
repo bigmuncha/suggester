50cb68163a6ccc4b7578a0a4360cb0e8a7b58f18
bigmuncha <mr.munchaev@mail.ru>
2021-02-05 05:41:44 +0300

    string set file

---
+++ stringset/data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import random
+
+my_file = open("some.txt", "r")
+liststr = my_file.readlines()
+for i in range(0,len(liststr)):
+    tempstr = random.randint(0,4294967296)
+    liststr[i] =  liststr[i][0:len(liststr[i])-1] +\
+        ' ' + str(tempstr)  + '\n'
+    print(liststr[i])
+my_file.close()
+with open("data.txt", "w") as new_file:
+    new_file.writelines(liststr)
